7ce1d82e87d81ee7bc8b48c6c6d22e9d4adfd709
esmemag96 <esmemag96@gmail.com>
2024-01-31 18:53:09 -0600

    merge readme

--- api/app.js
+++ api/app.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import { connect } from 'mongoose';
+import dotenv from 'dotenv';
 import pokemonRoutes from './routes/pokemon.js'; // Importing routes for Pokemon-related endpoints
 import favoriteRoutes from './routes/favorite.js'; // Importing routes for favorite Pokemon-related endpoints
 import { populateDatabase } from './services/pokeApi.js'; // Importing a service to populate the database with Pokemon data
 const app = express();
 const port = 3000;
-
+dotenv.config(); // Loading environment variables from .env file
 app.use(cors()); // Enabling CORS for all requests to allow cross-origin requests
 app.use(express.json()); // Middleware to parse JSON bodies
-
 // Connecting to MongoDB using mongoose
-connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.3', { useNewUrlParser: true, useUnifiedTopology: true })
+connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB')) // Logging on successful connection
   .catch(err => console.error('Failed to connect to MongoDB', err)); // Logging if connection fails
 
